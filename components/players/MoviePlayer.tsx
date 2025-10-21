dd3425a6e1cf2e8beb6be38a228e035bd21d8aef
Monish Bachani <110552309+monishbachani@users.noreply.github.com>
2025-10-21 11:51:07 +0530

    Update MoviePlayer.tsx

--- components/players/MoviePlayer.tsx
+++ components/players/MoviePlayer.tsx
@@ -16,7 +16,7 @@ export default function MoviePlayer({
   mediaId, 
   title, 
   onClose, 
-  backButton = 'show' 
+  backButton = 'hide' 
 }: MoviePlayerProps) {
   const [embedUrl, setEmbedUrl] = useState<string>('');
   const [loading, setLoading] = useState(true);
