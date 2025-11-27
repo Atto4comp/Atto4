f095a2ebd760cd45460352dfa557d43e6a6f0693
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-11-28 00:32:25 +0530

    Update video-tv.ts

--- lib/api/video-tv.ts
+++ lib/api/video-tv.ts
@@ -11,7 +11,7 @@ export interface TVEmbedResult {
 const PROVIDERS = [
   { url: "https://iframe.pstream.mov/embed/tmdb-tv-${id}/${season}/${episode}?logo=false&tips=false&theme=default&allinone=true&backlink=${backlink}", label: "Atto4" },
   { url: "https://vidfast.to/embed/tv/${id}/${season}/${episode}", label: "Vidfast" },
-  { url: "https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&logourl=https%3A%2F%2Fi.ibb.co%2F67wTJd9R%2Fpngimg-com-netflix-PNG11.png&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5", label: "VidPlus" }
+  { url: "https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5", label: "VidPlus" }
 ];
 
 export function getTVEmbed(id: string | number, season: number = 1, episode: number = 1): TVEmbedResult {
