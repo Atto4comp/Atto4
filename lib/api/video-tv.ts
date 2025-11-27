dcb8e83df0d3f0973e04a08499da99856b82c851
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-11-28 00:43:08 +0530

    Update video-tv.ts

--- lib/api/video-tv.ts
+++ lib/api/video-tv.ts
@@ -9,9 +9,9 @@ export interface TVEmbedResult {
 }
 
 const PROVIDERS = [
-  { url: "https://iframe.pstream.mov/embed/tmdb-tv-${id}/${season}/${episode}?logo=false&tips=false&theme=default&allinone=true&backlink=${backlink}", label: "Atto4" },
+  { url: "https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5", label: "Vidly" },
   { url: "https://vidfast.to/embed/tv/${id}/${season}/${episode}", label: "Vidme" },
-  { url: "https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5", label: "Vidly" }
+  { url: "https://iframe.pstream.mov/embed/tmdb-tv-${id}/${season}/${episode}?logo=false&tips=false&theme=default&allinone=true&backlink=${backlink}", label: "Atto4" }
 ];
 
 export function getTVEmbed(id: string | number, season: number = 1, episode: number = 1): TVEmbedResult {
