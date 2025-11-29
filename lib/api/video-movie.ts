4180bf24961b1478b4720e3b70cb215703b1abc2
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-11-30 01:53:14 +0530

    Update video-movie.ts

--- lib/api/video-movie.ts
+++ lib/api/video-movie.ts
@@ -10,7 +10,7 @@ export interface MovieEmbedResult {
 
 const PROVIDERS = [
   { url: "https://fmovies4u.com/embed/tmdb-movie-${id}?theme=16A085&hideServer=true&autoPlay=true&sub=en&nextButton=true&autoNext=true&title=false", label: "Vidly" },
-  { url: "https://bidsrc.pro/embed/movie/${id}", label: "vidzy" },
+  { url: "https://bidsrc.pro/movie/${id}?autoplay=true", label: "vidzy" },
   { url: "https://vidfast.to/embed/movie/${id}", label: "Vidme" }
 ];
 
