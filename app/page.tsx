58f8157054ba1454c4721af1dc669afa71a4300e
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-12-01 21:09:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -139,7 +139,7 @@ export default async function HomePage() {
       </Suspense>
       
       <meta name="google-adsense-account" content="ca-pub-6668961984680825">
-      <meta name="b2b4e492a079f757d4b5bb735a0b869a6c6db20e" content="b2b4e492a079f757d4b5bb735a0b869a6c6db20e" />
+      <meta name="b2b4e492a079f757d4b5bb735a0b869a6c6db20e" content="b2b4e492a079f757d4b5bb735a0b869a6c6db20e">
       
       {/* ✅ ADVERTISEMENT SLOT ADDED HERE */}
       <BannerAd />
