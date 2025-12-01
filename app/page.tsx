eb46fce97592ce16389b5c8c5181de6ad018f438
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-12-01 21:11:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -138,8 +138,8 @@ export default async function HomePage() {
         <HeroSection media={(trending || []).slice(0, 5)} />
       </Suspense>
       
-      <meta name="google-adsense-account" content="ca-pub-6668961984680825">
-      <meta name="b2b4e492a079f757d4b5bb735a0b869a6c6db20e" content="b2b4e492a079f757d4b5bb735a0b869a6c6db20e">
+      <meta name="google-adsense-account" content="ca-pub-6668961984680825"></meta>
+      <meta name="b2b4e492a079f757d4b5bb735a0b869a6c6db20e" content="b2b4e492a079f757d4b5bb735a0b869a6c6db20e"></meta>
       
       {/* ✅ ADVERTISEMENT SLOT ADDED HERE */}
       <BannerAd />
