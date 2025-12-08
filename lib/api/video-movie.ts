e8a8252fc6db6ce1919d07b6ada2d02fe3d49fe3
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-12-09 00:05:34 +0530

    Update video-movie.ts

--- lib/api/video-movie.ts
+++ lib/api/video-movie.ts
@@ -12,7 +12,8 @@ const PROVIDERS = [
   { url: "https://fmovies4u.com/embed/tmdb-movie-${id}?autoPlay=true&hideTitle=true", label: "Vidly" },
   { url: "https://api.cinezo.net/media/tmdb-movie-${id}", label: "Cinezo" },
   { url: "https://www.vidking.net/embed/movie/${id}?color=5865f2&autoPlay=true&nextEpisode=true&episodeSelector=true", label: "vidme" },
-  { url: "https://bidsrc.pro/movie/${id}?autoplay=true", label: "vidzy" }
+  { url: "https://bidsrc.pro/movie/${id}?autoplay=true", label: "vidzy" },
+  { url: "https://player.vidplus.to/embed/movie/${id}?autoplay=true&poster=true&title=true&watchparty=false&chromecast=true&servericon=true&setting=true&pip=true&hideprimarycolor=true&hidesecondarycolor=true&hideiconcolor=true&hideprogresscontrol=true&hideiconset=true&hideautonext=true&hideautoplay=true&hidenextbutton=true&hideposter=true&hidetitle=true&hidechromecast=true&hideepisodelist=true&hideservericon=true&hidepip=true&icons=netflix&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5", label: "vidon" }
     
 ];
 
