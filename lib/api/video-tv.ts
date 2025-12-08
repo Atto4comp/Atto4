f37510381c12229189b7535328d901bedadfe870
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-12-09 00:06:29 +0530

    Update video-tv.ts

--- lib/api/video-tv.ts
+++ lib/api/video-tv.ts
@@ -12,7 +12,8 @@ const PROVIDERS = [
   { url: "https://fmovies4u.com/embed/tmdb-tv-${id}/${season}/${episode}?autoPlay=true&hideTitle=true", label: "Vidly" },
   { url: "https://api.cinezo.net/media/tmdb-tv-${id}/${season}/${episode}", label: "Cinezo" },
   { url: "https://www.vidking.net/embed/tv/${id}/${season}/${episode}?color=5865f2&autoPlay=true&nextEpisode=true&episodeSelector=true", label: "Vidme" },
-  { url: "https://bidsrc.pro/tv/${id}/${season}/${episode}?autoplay=true", label: "Vidzy" }
+  { url: "https://bidsrc.pro/tv/${id}/${season}/${episode}?autoplay=true", label: "Vidzy" },
+  { url: "https://player.vidplus.to/embed/tv/${id}/${season}/${episode}?autoplay=true&autonext=true&nextbutton=true&poster=true&title=true&watchparty=false&chromecast=true&episodelist=true&servericon=true&setting=true&pip=true&hideprimarycolor=true&hidesecondarycolor=true&hideiconcolor=true&hideprogresscontrol=true&hideiconset=true&hideautonext=true&hideautoplay=true&hidenextbutton=true&hideposter=true&hidetitle=true&hidechromecast=true&hideepisodelist=true&hideservericon=true&hidepip=true&icons=netflix&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5", label: "Vidon" }
   
 ];
 
