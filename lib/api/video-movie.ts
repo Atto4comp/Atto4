90deafd608109f9c470df8f81de2a7cf20bae758
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-12-15 02:32:47 +0530

    Update video-movie.ts

--- lib/api/video-movie.ts
+++ lib/api/video-movie.ts
@@ -24,8 +24,8 @@ const SERVERS = [
     key: "L2Vpdm9tL29ycC5jcnNkaWIvLzpwYXR0aA==" 
   },
   { 
-    id: 'fmovies4u', 
-    label: 'Fmovies', 
+    id: 'vidly', 
+    label: 'Vidly', 
     // "https://fmovies4u.com/embed/tmdb-movie-"
     key: "LWVpdm9tLWJkbXQvZWJtZWQvbW9jLnU0c2Vpdm9tZi8vOnNwdHRo" 
   },
@@ -52,9 +52,6 @@ const CONFIGS: Record<string, string> = {
   cinezo: "",
   vidme: "?color=5865f2&autoPlay=true&nextEpisode=true&episodeSelector=true",
   vidzy: "?autoplay=true",
-  vidzee: "",
-  vidrock: "",
-  vidon: "?autoplay=true&poster=true&title=true&watchparty=false&chromecast=true&servericon=true&setting=true&pip=true&hideprimarycolor=true&hidesecondarycolor=true&hideiconcolor=true&hideprogresscontrol=true&hideiconset=true&hideautonext=true&hideautoplay=true&hidenextbutton=true&hideposter=true&hidetitle=true&hidechromecast=true&hideepisodelist=true&hideservericon=true&hidepip=true&icons=netflix&primarycolor=6C63FF&secondarycolor=9F9BFF&iconcolor=FFFFFF&font=Roboto&fontcolor=FFFFFF&fontsize=20&opacity=0.5"
 };
 
 export function getMovieEmbed(id: string | number): MovieEmbedResult {
