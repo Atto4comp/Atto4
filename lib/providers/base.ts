64441479ab8d5a896f7931c003ec48b0c8810c79
Atto4 <110552309+Atto4comp@users.noreply.github.com>
2025-12-14 17:42:15 +0530

    Update base.ts

--- lib/providers/base.ts
+++ lib/providers/base.ts
@@ -1,14 +1,38 @@
 import {
   makeProviders,
   makeStandardFetcher,
-  makeSimpleProxyFetcher,
   targets,
+  Fetcher,
 } from '@p-stream/providers';
 
 const PROXY = process.env.NEXT_PUBLIC_PROXY_URL!;
 
+// Custom proxied fetcher
+const proxyFetcher: Fetcher = async (url, ops) => {
+  const proxyUrl = new URL(PROXY);
+  proxyUrl.searchParams.set('url', url);
+
+  if (ops?.headers) {
+    proxyUrl.searchParams.set(
+      'headers',
+      encodeURIComponent(JSON.stringify(ops.headers))
+    );
+  }
+
+  const res = await fetch(proxyUrl.toString(), {
+    method: 'GET',
+  });
+
+  return {
+    body: await res.text(),
+    finalUrl: res.url,
+    headers: res.headers,
+    statusCode: res.status,
+  };
+};
+
 export const providers = makeProviders({
   fetcher: makeStandardFetcher(fetch),
-  proxiedFetcher: makeSimpleProxyFetcher(PROXY, fetch),
+  proxiedFetcher: proxyFetcher,
   target: targets.BROWSER,
 });
